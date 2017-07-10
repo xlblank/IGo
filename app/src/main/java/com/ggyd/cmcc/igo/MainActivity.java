af813e8575456a669ad136b8b3499c6356882567
xlblank <18807859308@139.com>
2017-07-10 14:04:39 +0800

    线性布局完成底部按钮。

--- app/src/main/java/com/ggyd/cmcc/igo/MainActivity.java
+++ app/src/main/java/com/ggyd/cmcc/igo/MainActivity.java
@@ -1,13 +1,22 @@
 package com.ggyd.cmcc.igo;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
     }
+
+
+
 }
